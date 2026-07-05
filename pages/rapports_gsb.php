7e6b4feb7f4ed4c7375395ff094be0331bb04463
Ruth Axelle DON <donruthaxelle@gmail.com>
2026-07-05 22:01:11 +0000

    Fix: ajouter require notifications.php manquant

--- pages/rapports_gsb.php
+++ pages/rapports_gsb.php
@@ -12,6 +12,7 @@ require_once __DIR__ . '/../includes/db.php';
 require_once __DIR__ . '/../includes/session.php';
 require_once __DIR__ . '/../includes/helpers.php';
 require_once __DIR__ . '/../includes/audit.php';
+require_once __DIR__ . '/../includes/notifications.php';
 
 require_auth();
 
